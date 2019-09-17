b40d32e10467304485440826fa67f660f21a9cd0
QED0711 <qdizon@gmail.com>
2019-09-17 10:52:52 -0400

    added plot function for total locations count

--- utils/barplots.py
+++ utils/barplots.py
@@ -21,3 +21,21 @@ def make_barplots(df, features, save=False):
         else:
             fig.show()
         
+
+def total_locations(state_count_df, save=False):
+    df = state_count_df.groupby("building_name").sum()
+
+    fig = go.Figure(
+        data=go.Bar(
+            x=[x.title() for x in df.index],
+            y=df.total_in_state,
+            marker_color=['#FC3D38', '#074C70', '#1892D1', '#F2625E', '#F4D00A', '#DFA025']
+        )
+    )
+
+    if save:
+        plotly.io.write_html(fig, f"../visuals/total_in_state-bar.html")
+    else:
+        fig.show()
+    
+
